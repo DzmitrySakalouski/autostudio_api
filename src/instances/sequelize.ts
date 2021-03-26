24fc3c46c84ded0153d4596428d6323ac8c15786
DzmitrySakalouski <dmitry89sokol@gmail.com>
2021-03-26 14:12:26 +0300

    add db connection

--- src/instances/sequelize.ts
+++ src/instances/sequelize.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize-typescript';
 
-export const sequelize = new Sequelize({
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   database: 'autostudiodb',
   dialect: 'postgres',
   pool: {
