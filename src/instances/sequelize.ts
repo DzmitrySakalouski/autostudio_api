0232f1bdb77cdf05148ff29e8a2df778463d05e9
DzmitrySakalouski <dmitry89sokol@gmail.com>
2021-03-26 14:53:25 +0300

    add database creds

--- src/instances/sequelize.ts
+++ src/instances/sequelize.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize-typescript';
 
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  database: 'autostudiodb',
+  database: 'd5c59fhi9pib4t',
   dialect: 'postgres',
   dialectOptions: {
     ssl: true,
@@ -12,8 +12,8 @@ export const sequelize = new Sequelize(process.env.DATABASE_URL, {
     acquire: 30000,
     idle: 10000
   },
-  username: 'postgres',
-  password: 'sokill666',
+  username: 'ssitzwrjwtuidn',
+  password: 'd20983e9001d8440f051ef20fdbcfdc0abe689664fee7d293a10d22ef33323b0',
   storage: ':memory:',
   models: [__dirname + '/models'] // or [Player, Team],
 });
